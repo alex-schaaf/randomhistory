a56f792eaec42228510f09d58920e9229640c461
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-07-06 14:47:10 +0200

    Add event dict prototypes for convenience

---
+++ randomhistory/prototypes.py
@@ -0,0 +1,61 @@
+stratigraphy = {
+    'num_layers': 0,
+    'layer_names': [],
+    'layer_thickness': []
+}
+
+tilt = {
+    "name": "Event Name",
+    "pos": (0, 0, 0),
+    "rotation": 5,
+    "plunge_direction": 90,
+    "plunge": 5
+}
+
+fault_planar = {
+    "name": "Event Name",
+    "pos": (0, 0, 0),
+    "dip_dir": 90,
+    "dip": 60,
+    "slip": 100
+}
+
+fault_curved = {
+    "name": "Event Name",
+    'geometry': 'Curved',
+    "pos": (0, 0, 0),
+    "dip_dir": 90,
+    "dip": 60,
+    "slip": 100,
+    "amplitude": 1000,
+    "radius": 2000,
+    'xaxis': 5000.,
+    'yaxis': 5000.0,
+    'zaxis' : 39999.0
+}
+
+fold = {
+    "name": "Event Name",
+    "pos": (0, 0, 0),
+    "wavelength": 1000,
+    "amplitude": 50,
+    "plunge_direction": 90,
+    "plunge": 0
+}
+
+unconformity_empty = {
+    "name": "Event Name",
+    "pos": (0, 0, 0),
+    "dip_direction": 90,
+    "dip": 5,
+}
+
+unconformity_layers = {
+    "name": "Event Name",
+    "pos": (0, 0, 0),
+    "dip_direction": 90,
+    "dip": 5,
+    "num_layers": 0,
+    "layer_names": [],
+    "layer_thickness": []
+}
